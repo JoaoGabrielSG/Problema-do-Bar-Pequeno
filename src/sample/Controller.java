2f828da013a7fd871ceec57d6ec7a987e3e09683
Gabriel Santos <joaogabrieldosantosgoncalves@gmail.com>
2017-05-07 16:23:24 -0300

    Add a button and create a reference for this button in the code

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class Controller{
+    @FXML private Button myButton;
+
+    @FXML
+    public void initialize() {
+
+    myButton.setDisable(true);
+
+    }
 }
