2da88a5f10fd1e75b3ce4d6dee60fd6e2b912b85
Gabriel Santos <joaogabrieldosantosgoncalves@gmail.com>
2017-05-13 18:49:24 -0300

    Add class ClientThread

---
+++ src/sample/ClientThread.java
@@ -0,0 +1,11 @@
+package sample;
+
+/**
+ * Created by João Gabriel on 13/05/2017.
+ */
+public class ClientThread extends Thread {
+
+    public ClientThread(String name) {
+        super(name);
+    }
+}
