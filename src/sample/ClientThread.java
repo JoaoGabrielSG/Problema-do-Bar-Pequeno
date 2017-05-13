e105c0b4d93fcf738669cde84616cafe945c40fc
Gabriel Santos <joaogabrieldosantosgoncalves@gmail.com>
2017-05-13 19:12:27 -0300

    Add constructor

--- src/sample/ClientThread.java
+++ src/sample/ClientThread.java
@@ -1,11 +1,23 @@
 package sample;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * Created by João Gabriel on 13/05/2017.
  */
 public class ClientThread extends Thread {
 
-    public ClientThread(String name) {
+    int tempo_bar;
+    int tempo_casa;
+
+    Semaphore mutex1 = new Semaphore(1);
+
+
+
+    public ClientThread(String name, int tempo_bar, int tempo_casa) {
         super(name);
+
+        this.tempo_bar = tempo_bar;
+        this.tempo_casa = tempo_casa;
     }
 }
