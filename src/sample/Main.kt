14670bff94ccf3d6271b65af2e0934f82fe151ab
Matheus Martins <matheusmcrds@gmail.com>
2017-05-17 08:40:41 -0300

    fxgl start

---
+++ src/sample/Main.kt
@@ -0,0 +1,11 @@
+package barproblem
+
+/**
+ * Created by matheusmartins on 5/17/17.
+ */
+
+fun main(args : Array<String>) {
+    val viewController = ViewController()
+    viewController.main(args)
+    //viewController.
+}
--- src/sample/ViewController.kt
+++ src/sample/ViewController.kt
@@ -17,13 +17,69 @@ import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.layout.Pane
 import javax.swing.plaf.RootPaneUI
+import javafx.scene.paint.*
+import com.almasb.fxgl.app.*
+import com.almasb.fxgl.settings.*
+import com.almasb.fxgl.input.*
+import javafx.application.Application
+import javafx.scene.input.KeyCode
 
-class ViewController {
+
+
+class ViewController: GameApplication() {
     @FXML
     private lateinit var myButton: Button
 
     @FXML
     private lateinit var test: Button
+
+    override fun initSettings(p0: GameSettings?) {
+        p0?.width = 1280
+        p0?.height = 720
+        p0?.title = "pitombeira simulator 2017"
+        p0?.version = "0.1.0 [alpha] "
+        p0?.isProfilingEnabled = false
+        p0?.isIntroEnabled = false
+        p0?.isMenuEnabled = false
+        p0?.isFullScreen = false
+        p0?.applicationMode = ApplicationMode.DEVELOPER
+    }
+
+    override fun initInput() {
+        input.addAction(object : UserAction("Jump") {
+            override fun onActionBegin() {
+                print("jumo")
+            }
+        }, KeyCode.SPACE)
+    }
+
+    override fun initGameVars(vars: MutableMap<String, Any>?) {
+        vars?.put("stageColor", Color.BLACK)
+    }
+
+    override fun initGame() {
+        //initBackground();
+        //initPlayer();
+
+        //initBackgroundMusic();
+    }
+
+    override fun initUI() {
+        val uiScore = uiFactory.newText("", 72.0)
+        uiScore.translateX = (width - 200.0)
+        uiScore.translateY = 50.0
+        //uiScore.fillProperty().bind(gameState.objectProperty<T>("stageColor"))
+        //uiScore.textProperty().bind(getMasterTimer().tickProperty().asString());
+
+        gameScene.addUINode(uiScore)
+    }
+
+    override fun onPostUpdate(tpf: Double) {
+
+    }
+
+
+
 
     @FXML
     fun initialize() {
@@ -63,5 +119,9 @@ class ViewController {
         }
 
         myButton.setOnAction { runClients() }
+    }
+
+    fun main(args: Array<String>) {
+        Application.launch(*args)
     }
 }
